80f16bf6df0440d763867b488042246ee7f5399f
Simon Pratt <simon@pr4tt.com>
2016-03-14 21:44:02 -0400

    Added header protection to radixtree

--- radixtree.hpp
+++ radixtree.hpp
@@ -1,3 +1,5 @@
+#ifndef RADIXTREE_H
+#define RADIXTREE_H
 /******************************************************************************\
  * Radix Tree
  * MIT License
@@ -173,3 +175,50 @@ public:
   }
   size_t size() { return s_; }
 };
+
+// Given a 64-bit number vadd, returns the same number but with the
+// most-significant 16-bits (63:48) replaced with the value of bit 47,
+// effectively sign-extending it to make it a valid virtual address in
+// x86_64.
+//
+// 6  6         5  4      4         3         2         1         0
+// 3210987654321098765432109876543210987654321098765432109876543210
+// ----------------------------------------------------------------
+// \______________/\_______/\_______/\_______/\_______/\__________/
+//  sign extend(16)  p4(9)    p3(9)    p2(9)    p1(9)   offset(12)
+//
+uint64_t correctify_vadd(uint64_t vadd) {
+  // 48 0'x
+  uint64_t bit47 = 0b1;
+  bit47 <<= 47;
+  if(vadd & bit47) {
+    uint64_t mask = 0b1111111111111111;
+    mask <<= 48;
+    return vadd | mask;
+  } else {
+    uint64_t mask = 0b111111111111111111111111111111111111111111111111;
+    return vadd & mask;
+  }
+}
+
+// Given 64-bit numbers vadd and padd, return a valid physical address
+// with the following form:
+//
+// 6  6         5  4      4         3         2         1         0
+// 3210987654321098765432109876543210987654321098765432109876543210
+// ----------------------------------------------------------------
+// 0000000000000000000000001000000010000000011000000100000000000000  <- example
+// \__________/\______________________________________/\__________/
+//  ignore(12)                padd(40)                   vadd(12)
+//
+// That is, return the least-significant 12 bits (11:0) from vadd
+// bitwise-ORed with bits 47:12 from padd.
+//
+uint64_t correctify_padd(uint64_t vadd, uint64_t padd) {
+  uint64_t vmask = 0b111111111111;
+  uint64_t pmask = 0b1111111111111111111111111111111111111111;
+  return ((pmask << 12) & padd) | (vmask & vadd);
+}
+
+/******************************************************************************/
+#endif
